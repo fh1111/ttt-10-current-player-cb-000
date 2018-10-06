3feaa205e7a6be813ea2e237d1892ed170fca126
Fer Hernandez <fh.1111@gmail.com>
2018-10-06 20:26:27 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,14 @@
-
-
-
 def turn_count(board)
 
+  counter=0
+  board.each do |element|
 
+    if (element=='X' || element=='O')
+      counter+=1
+      end
+
+    end
+  puts "#{counter}"
 
 end
 
@@ -16,4 +21,4 @@ def current_player()
 end
 
 
-#turn_count(["X","","O"])
+turn_count(["X","","O"])
