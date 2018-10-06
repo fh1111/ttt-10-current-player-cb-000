1c522eee335414f4834355e0c58df9c841c126d1
Fer Hernandez <fh.1111@gmail.com>
2018-10-06 20:14:13 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,10 +3,11 @@ def turn_count (board)
   counter=0
   board.each do |element|
 
-    if (elment=='X' || element=='O')
+    if (element=='X' || element=='O')
       counter+=1
     end
   end
+
 end
 
 
